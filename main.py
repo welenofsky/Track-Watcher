8c3e70f1a97a29aff9ef319a88201fd64b8c53e2
Justin Welenofsky <welenofsky@gmail.com>
2016-09-08 23:14:08 -0700

    Tells you if there is a new track, needs refactoring

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 #!/usr/bin/python3
 
 import urllib.request
+import json
+import time
 
 
 def get_tracks(user, client_id):
@@ -24,6 +26,72 @@ def get_tracks(user, client_id):
 	return content.decode("utf-8")
 
 
+def get_last_track_history_time():
+	track_history = None
+
+	try:
+		track_history = open('history.txt', 'r')
+	except FileNotFoundError:
+		try:
+			track_history = open('history.txt', 'w+')
+		except:
+			print("Could not open track history file for writing\n")
+
+	last_track_timestamp = track_history.read()
+
+	if last_track_timestamp:
+		return last_track_timestamp
+
+	return None
+
+
+def set_last_track_history_time(created_at):
+	if created_at is None:
+		return None
+
+	track_history = None
+
+	try:
+		track_history = open('history.txt', 'w+')
+	except:
+		print("Could not open track history file for writing\n")
+
+	track_history.write(created_at)
+
+	return None
+
+
+def get_last_track_datetime(tracks):
+	if tracks is None:
+		return None
+
+	parsed_tracks = json.loads(tracks)
+
+	if len(parsed_tracks) > 0:
+		last_track = parsed_tracks[0]
+
+	soundcloud_timestamp 	= last_track["created_at"]
+	track_title 			= last_track["title"]
+
+	if soundcloud_timestamp:
+		try:
+			current_track_created_at 	= time.strptime(soundcloud_timestamp, "%Y/%m/%d %H:%M:%S %z")
+			last_track_created_at 		= get_last_track_history_time()
+
+			if last_track_created_at:
+				last_track_created_at = time.strptime(last_track_created_at, "%Y/%m/%d %H:%M:%S %z")
+				if last_track_created_at <= current_track_created_at:
+					print("No new tracks at this time")
+					exit()
+			print(repr(last_track_created_at))
+			print(repr(current_track_created_at))
+			print("NEW TRACK: " + track_title)
+			set_last_track_history_time(soundcloud_timestamp)
+
+		except ValueError as e:
+			print("bad timestamp received from soundcloud, exiting...\nError: " + e)
+			exit()
+
 def main():
 	secrets = None
 
@@ -33,13 +101,12 @@ def main():
 		print("Could not find secrets.txt, exiting")
 		exit()
 
-
 	if secrets:
 		client_id 	= secrets.read()
 		tracks 		= get_tracks("ytcracker", client_id)
 
 		if tracks:
-			print(tracks)
+			get_last_track_datetime(tracks)
 		else:
 			print("Could not find any tracks")
 
