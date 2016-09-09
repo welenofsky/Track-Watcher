53c0f20f6a2427a0f34aa8ee0703956fcf36a199
Justin Welenofsky <welenofsky@gmail.com>
2016-09-08 22:26:12 -0700

    Catch errors

--- main.py
+++ main.py
@@ -1,20 +1,34 @@
 #!/usr/bin/python3
+
 import urllib.request
 
 
 def get_tracks(user, client_id):
-	api_endpoint 	= "http://api.soundcloud.com/users/:user/tracks?client_id=:client_id"
-	api_endpoint 	= api_endpoint.replace(':user', user)
-	api_endpoint 	= api_endpoint.replace(':client_id', client_id)
-	page			= urllib.request.urlopen(api_endpoint)
+	api_endpoint = "http://api.soundcloud.com/users/{user}/tracks?client_id={client_id}"
+	api_endpoint = api_endpoint.format(
+		user		= user,
+		client_id	= client_id
+	)
 
-	return page.read().decode('utf-8')
+	try:
+		page = urllib.request.urlopen(api_endpoint)
+	except URLError as e:
+		print("Could not connet to soundcloud, error: " + e)
+		return None
+
+	content = page.read()
+
+	if content is None:
+		return None
+
+	return content.decode("utf-8")
+
 
 def main():
 	secrets = None
 
 	try:
-		secrets = open('secrets.txt', 'r')
+		secrets = open("secrets.txt", "r")
 	except FileNotFoundError:
 		print("Could not find secrets.txt, exiting")
 		exit()
@@ -22,8 +36,11 @@ def main():
 
 	if secrets:
 		client_id 	= secrets.read()
-		tracks 		= get_tracks('ytcracker', client_id)
+		tracks 		= get_tracks("ytcracker", client_id)
 
-		print(tracks)
+		if tracks:
+			print(tracks)
+		else:
+			print("Could not find any tracks")
 
 main()
